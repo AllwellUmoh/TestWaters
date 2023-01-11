c5d08bd357bc191a5f6c83c2e3ad92f313a481c4
AllwellUmoh <allwellumoh1@gmail>
2023-01-11 03:08:06 -0800

    new look

--- 0x04-python-more_data_structures/2-uniq_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def uniq_add(my_list=[]):
-    if my_list is not None:
-        result = 0
-        for f in sorted(set(my_list)):
-            result += f
-        return result
